86ce0f32ad1b7410f4a780466afb7104c0be05ee
RafaelVolkmer <85331378+RafaelVVolkmer@users.noreply.github.com>
2022-07-07 20:24:50 -0300

    Update Teclado.c

--- I/Segundo Trimestre/Teclado/Teclado.c
+++ I/Segundo Trimestre/Teclado/Teclado.c
@@ -1,173 +1,159 @@
 //Rafael V. Volkmer - 4324 / N°21 | Teclado Musical
 #include "stm32f4xx.h"
+#include <math.h>
 
-#define FrequenciaPSC (4000-1)
-#define FrequenciaARR (1000-1)
-#define mascara 0b11111111
+#define FrequenciaPSC (1000-1)
+#define FrequenciaARR (160-1)
+#define mascara 0b111111111111
 
-uint8_t tempo;
-uint8_t teclas;
-uint8_t teclas_sustenidas;
-uint8_t nota;
+volatile uint8_t tempo;
+volatile uint16_t teclas;
+volatile uint32_t *nota;
 
-enum {DO=1,RE,MI=4,FA=8,SOL=16,LA=32,SI=64};
-enum {DOs=1,REs,MIs=4,FAs=8,SOLs=16,LAs=32,SIs=64};
+enum {DO=1, DOs=2, RE=4, REs=8, MI=16, FA=32, FAs=64, SOL=128, SOLs=256, LA=512, LAs=1024, SI=2048};
 
 //Função para ativar saída com o botão pressionado
 void chaveamento()
 {
-	if (teclas)
-			 {
-				 if (TIM10->SR & TIM_SR_UIF)
-				 {
-					GPIOA->ODR^=GPIO_ODR_ODR_7;
-				 }
-			 }
-			 else
-					GPIOA->ODR&=~GPIO_ODR_ODR_7;
+	teclas = GPIOC->IDR & mascara;
+	tempo = TIM10->SR & TIM_SR_UIF;
+	
+	if(teclas)
+	{
+		if(tempo)
+		{
+			TIM10->SR =~ TIM_SR_UIF;
+			GPIOA->ODR^=GPIO_ODR_ODR_7;
+		}
+	}
 }
+
+int frequenciaNota(int nota)
+{
+	const float constanteNotas = 1.0594631;
+	const float doPrimeiraEscala = 261.626;
+
+	const int frequenciaNucleo = 16000000;
+	int frequenciaNota, arr;
+
+	frequenciaNota = doPrimeiraEscala*pow(constanteNotas, nota-1);
+
+	arr = (frequenciaNucleo/FrequenciaPSC)/frequenciaNota;
+
+	return arr;
+}
+
+//
 
 int main(void)
 {
 
 	//Ativando clock GPIOA e TIMER10
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOCEN;
 	RCC->APB2ENR |= RCC_APB2ENR_TIM10EN;
 
-	//PINOS GPIOC - ENTRADAS DAS NOTAS NORMAIS.
+	//PINOS GPIOC - ENTRADA DAS NOTAS
 	GPIOC->MODER &=~
 		(
-			GPIO_MODER_MODER0 |
-			GPIO_MODER_MODER1 |
-			GPIO_MODER_MODER2 |
-			GPIO_MODER_MODER3 |
-			GPIO_MODER_MODER4 |
-			GPIO_MODER_MODER5 |
-			GPIO_MODER_MODER6
+			GPIO_MODER_MODER0  |
+			GPIO_MODER_MODER1  |
+			GPIO_MODER_MODER2  |
+			GPIO_MODER_MODER3  |
+			GPIO_MODER_MODER4  |
+			GPIO_MODER_MODER5  |
+			GPIO_MODER_MODER6  |
+			GPIO_MODER_MODER7  |
+			GPIO_MODER_MODER8  |
+			GPIO_MODER_MODER9  |
+			GPIO_MODER_MODER10 |
+			GPIO_MODER_MODER11
 		);
 
-	GPIOC->MODER |=
-
-			GPIO_MODER_MODER1_0 |
-			GPIO_MODER_MODER2_0 |
-			GPIO_MODER_MODER3_0 |
-			GPIO_MODER_MODER4_0 |
-			GPIO_MODER_MODER5_0 |
-			GPIO_MODER_MODER6_0 |
-			GPIO_MODER_MODER7_0;
-
-	//PINOS GPIOB - ENTRADAS DAS NOTAS SUSTENIDAS.
-	GPIOB->MODER &=~
+	// DEFININDO PULL DOWN GPIOC
+	GPIOC->PUPDR |=
 		(
-			GPIO_MODER_MODER0 |
-			GPIO_MODER_MODER1 |
-			GPIO_MODER_MODER2 |
-			GPIO_MODER_MODER3 |
-			GPIO_MODER_MODER4 |
-			GPIO_MODER_MODER5 |
-			GPIO_MODER_MODER6
+			GPIO_PUPDR_PUPDR0_1  |
+			GPIO_PUPDR_PUPDR1_1  |
+			GPIO_PUPDR_PUPDR2_1  |
+			GPIO_PUPDR_PUPDR3_1  |
+			GPIO_PUPDR_PUPDR4_1  |
+			GPIO_PUPDR_PUPDR5_1  |
+			GPIO_PUPDR_PUPDR6_1  |
+			GPIO_PUPDR_PUPDR7_1  |
+			GPIO_PUPDR_PUPDR8_1  |
+			GPIO_PUPDR_PUPDR9_1  |
+			GPIO_PUPDR_PUPDR10_1 |
+			GPIO_PUPDR_PUPDR11_1
 		);
 
-		GPIOB->MODER |=
-
-			GPIO_MODER_MODER1_0 |
-			GPIO_MODER_MODER2_0 |
-			GPIO_MODER_MODER3_0 |
-			GPIO_MODER_MODER4_0 |
-			GPIO_MODER_MODER5_0 |
-			GPIO_MODER_MODER6_0 |
-			GPIO_MODER_MODER7_0;
-
-	//SAÍDA DO SOM - PA7.
-	GPIOA->MODER &=~ GPIO_MODER_MODER7;
+	// PINO PA7 - SAÍDA DO SOM
+	GPIOA->MODER |= GPIO_MODER_MODER7_0;
 
 	//Definindo frequências para o TIMER10
 	TIM10->PSC = FrequenciaPSC;
-	TIM10->ARR= FrequenciaARR;
+	TIM10->ARR = FrequenciaARR;
 
 	//Habilitando contagem do TIMER10
 	TIM10->CR1 = TIM_CR1_CEN|TIM_CR1_ARPE;
 
-  while (1)
-  {
-	  //Mascaramentos
-	  teclas = GPIOC->IDR & mascara;
-	  teclas_sustenidas = GPIOB->IDR & mascara;
-	  tempo = TIM10->SR & TIM_SR_UIF;
-	  
-	  nota = TIM10->ARR;
+	while (1)
+	{
+		//Mascaramentos
+		teclas = GPIOC->IDR & mascara;
 
-	  chaveamento();
+		nota = &(TIM10->ARR);
 
-		 switch(teclas)
-		 {
-		  	  case DO:
-		  		nota=1; //*calcular frequências em ARR para chegar nas notas
-		  		    break;
+		chaveamento();
 
-		  	 case RE:
-		  		nota=1;
+		switch(teclas)
+		{
+			case DO:
+				*nota=frequenciaNota(1);
+			    	break;
+
+			case DOs:
+				*nota=frequenciaNota(2);
 		  			break;
 
-		  	 case MI:
-		  		nota=1;
+			case RE:
+				*nota=frequenciaNota(3);
 		  			break;
 
-		  	 case FA:
-		  		nota=1;
+			case REs:
+				*nota=frequenciaNota(4);
 		  			break;
 
-		  	 case SOL:
-		  		nota=1;
+			case MI:
+				*nota=frequenciaNota(5);
 		  			break;
 
-		  	 case LA:
-		  		nota=1;
+			case FA:
+				*nota=frequenciaNota(6);
 		  			break;
 
-		  	 case SI:
-		  		nota=1;
+			case FAs:
+				*nota=frequenciaNota(7);
 		  			break;
 
-		  	  default:
-		  		  nota=0;
-		  		  	break;
+			case SOL:
+				*nota=frequenciaNota(8);
+		  			break;
+
+			case SOLs:
+				*nota=frequenciaNota(9);
+		  			break;
+
+			case LA:
+				*nota=frequenciaNota(10);
+		  			break;
+
+			case LAs:
+				*nota=frequenciaNota(11);
+		  			break;
+
+			case SI:
+				*nota=frequenciaNota(12);
+		  			break;
 		 }
-
-		 switch(teclas_sustenidas)
-		 {
-		 		  	 case DOs:
-		 		  		nota=1;
-		 		  		    break;
-
-		 		  	 case REs:
-		 		  		nota=1;
-		 		  			break;
-
-		 		  	 case MIs:
-		 		  		nota=1;
-		 		  			break;
-
-		 		  	 case FAs:
-		 		  		nota=1;
-		 		  			break;
-
-		 		  	 case SOLs:
-		 		  		nota=1;
-		 		  			break;
-
-		 		  	 case LAs:
-		 		  		nota=1;
-		 		  			break;
-
-		 		  	 case SIs:
-		 		  		nota=1;
-		 		  			break;
-
-		 		  	  default:
-		 		  		  nota=0;
-		 		  		  	break;
-		 }
-
-  }
+	}
 }
