6a1368f99ff9e0223765f2b24ffbcd91bc3dbbb6
RafaelVolkmer <Rafael.V.Volkmer@gmail.com>
2022-10-15 19:07:49 -0300

    Update TecladoMatricial.h

--- I/Trabalho Multiplexação/TecladoMatricial.h
+++ I/Trabalho Multiplexação/TecladoMatricial.h
@@ -1,142 +1,151 @@
-/*
-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-				    BIBLIOTECA "TecladoMatricial.h"
-------------------------------------------------------------------------------------------------------------
-			    Feito por: Rafael V. Volkmer - ft. Pedro Trento
-____________________________________________________________________________________________________________
 
-	Essa biblioteca foi feita para a realização do trabalho de demultiplexação dado a mim e minha dupla,
-Pedro Trento, na disciplinas de Micros I, da Fundação Escola Técnica Liberato Salzano Vieira da Cunha.
-	Aqui, há funções para ler e captar valores das teclas pressionadas pelo teclado matricial, dados pela
-variável "Teclas". Para poder mexer certo com o hardwer, os pinos devem ser configurados defidamente na main,
-assim como suas conexões com os pinos do arm devem ser corretas. As entradas devem estar em PULL-UP e as
-saídas em OPEN - DRAIN. Segue a configuração abaixo:
+#ifndef TECLADOMATRICIAL_H_
+#define TECLADOMATRICIAL_H_
 
-------------------------------------------------------------------------------------------------------------
-					    TECLADO MATRICIAL
-------------------------------------------------------------------------------------------------------------
-
-                                           C0    C1    C2    C3
-          	  	  	  	  [PB4] [PB5] [PB6] [PB7]
-			       L0  [PB0] |  1  |  2  |  3  |  A  |
-			       L1  [PB1] |  4  |  5  |  6  |  B  |
-			       L2  [PB2] |  7  |  8  |  9  |  C  |
-			       L3  [PB3] |  *  |  0  |  #  |  D  |
-
-------------------------------------------------------------------------------------------------------------
-	  *Seguir o esquema acima é de suma importância para o bom funcionamento do código*
-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
- */
 
 #include "stm32f4xx.h"
 
-#define Timer TIM10->SR  //Aqui, pode mudar o timer que deseja usar (precisa ser compatível com o da sua main)
+#define Pinos_Colunas 0b11110000
+#define Pinos_Linhas 0b1111
 
-#define Numero_Colunas 4  //Aqui, pode mudar o número de colunas que tem em seu teclado
+#define Primeira_Coluna 0b11100000
+#define Segunda_Coluna 0b11010000
+#define Terceira_Coluna 0b10110000
+#define Quarta_Coluna 0b01110000
 
-#define Pinos_Colunas 0b11110000  //Aqui, pode mudar os pinos das colunas
-#define Pinos_Linhas 0b00001111   //Aqui, pode mudar os pinos das Linas
+#define Primeira_Linha 0b1110
+#define Segunda_Linha 0b1101
+#define Terceira_Linha 0b01011
+#define Quarta_Linha 0b0111
 
 #define Nenhuma_Tecla '\0'
 
-uint16_t Leitura[Numero_Colunas];
-uint8_t coluna = 0;
-uint16_t Tecla;
-
-void VarreduraColunas ()
+char VarreduraTeclado ()
 {
 
-	const uint16_t Varredura[Numero_Colunas] =
+	char tecla = Nenhuma_Tecla;
+
+	uint16_t i = 0;
+
+	GPIOB->ODR |= Pinos_Colunas;
+
+	GPIOB->ODR &= Primeira_Coluna;
+
+	i = GPIOB-> IDR & Pinos_Linhas;
+
+	if(i != Pinos_Linhas)
+	{
+		switch(i)
 		{
-				0b11101111,
-				0b11011111,
-				0b10111111,
-				0b01111111
-		};
+		case Primeira_Linha:
+			tecla = '1';
+			break;
 
-	   TIM10->SR &=~ TIM_SR_UIF;
+		case Segunda_Linha:
+			tecla = '4';
+			break;
 
-	   	   GPIOB->ODR |= Pinos_Colunas;
+		case Terceira_Linha:
+			tecla = '7';
+			break;
 
-	   	   GPIOB->ODR &= Varredura[coluna];
+		case Quarta_Linha:
+			tecla='*';
+			break;
+		}
+	}
 
-	   	   Leitura[coluna] = GPIOB->IDR | Pinos_Linhas;
+	GPIOB->ODR |= Pinos_Colunas;
 
-}
+	GPIOB->ODR &= Segunda_Coluna;
 
+	i = GPIOB->IDR & Pinos_Linhas;
 
-void IdentificaTeclas ()
-{
-
-	uint16_t ColunaMatriz;
-
-	const uint16_t Teclado_Matricial [4] [4] =
+	if(i != Pinos_Linhas)
+	{
+		switch(i)
 		{
-				{'1','2','3','A'},
-				{'4','5','6','B'},
-				{'7','8','9','C'},
-				{'*','0','#','D'}
-		};
 
+		case Primeira_Linha:
+			tecla = '2';
+			break;
 
-	if (++coluna == Numero_Colunas)
-	   	   {
+		case Segunda_Linha:
+			tecla = '5';
+			break;
 
-	   		   coluna = 0;
+		case Terceira_Linha:
+			tecla = '8';
+			break;
 
-	   		   	   for(ColunaMatriz = 0 ; ColunaMatriz < Numero_Colunas ; ColunaMatriz++)
-	   		   	   {
+		case Quarta_Linha:
+			tecla = '0';
+			break;
+		}
+	}
 
-	   		   		   	  switch(Leitura[ColunaMatriz])
-	   		   		   	  {
-                                                                         // C0  C1  C2  C3
-	   		   		   	  	  case 0b1110 : // [1] [2] [3] [A] - L0
-	   		   		   	  		  	  Tecla = Teclado_Matricial [0] [ColunaMatriz];
-	   		   		   	  		  	  	  break;
+	GPIOB->ODR |= Pinos_Colunas;
 
-	   		   		   	  		  	 // C0  C1  C2  C3
-	   		   		   	  	  case 0b1101 : // [4] [5] [6] [B] - L1
-	   		   		   	  		  	  Tecla = Teclado_Matricial [1] [ColunaMatriz];
-	   		   		   	  		  	  	  break;
+	GPIOB->ODR &= Terceira_Coluna;
 
-	   		   		   	  		         // C0  C1  C2  C3
-	   		   		   	  	  case 0b1011 : // [7] [8] [9] [C] - L2
-	   		   		   	  		  	  Tecla = Teclado_Matricial [2] [ColunaMatriz];
-	   		   		   	  		  	  	  break;
+	i = GPIOB->IDR & Pinos_Linhas;
 
-	   		   		   	  		         // C0  C1  C2  C3
-	   		   		   	  	  case 0b0111 : // [*] [0] [#] [D] - L3
-	   		   		   	  		  	 Tecla = Teclado_Matricial [3] [ColunaMatriz];
-	   		   		   	  		  	 	 break;
+	if(i != Pinos_Linhas)
+	{
+		switch(i)
+		{
+			case Primeira_Linha:
+				tecla = '3';
 
-	   		   		   	  }
+					break;
 
-	   		   	   }
+			case Segunda_Linha:
+				tecla = '6';
 
-	   	   }
+					break;
+
+			case Terceira_Linha:
+				tecla = '9';
+					break;
+
+		case Quarta_Linha:
+			tecla='#';
+			break;
+		}
+	}
+
+	GPIOB->ODR |= Pinos_Colunas;
+
+	GPIOB->ODR &= Quarta_Coluna;
+
+	i = GPIOB->IDR & Pinos_Linhas;
+
+	if(i != Pinos_Linhas)
+	{
+		switch(i)
+		{
+		case Primeira_Linha:
+			tecla = 'A';
+
+			break;
+
+		case Segunda_Linha:
+			tecla = 'B';
+			break;
+
+		case Terceira_Linha:
+			tecla = 'C';
+			break;
+
+		case Quarta_Linha:
+			tecla = 'D';
+			break;
+		}
+	}
+
+	return tecla;
+
 }
 
-void SemTeclaAtiva ()
-{
-	if (Tecla != Nenhuma_Tecla)
-	   	   {
-	   		   Tecla = Nenhuma_Tecla;
-	   	   }
-}
 
-void TecladoMatricial_Completo ()
-{
-	uint8_t tempo = Timer & TIM_SR_UIF;
-
-	   if (tempo)
-	   {
-
-		   VarreduraColunas ();
-
-		   IdentificaTeclas ();
-
-	   }
-
-	   	   SemTeclaAtiva ();
-
-}
+#endif /* TECLADOMATRICIAL_H_ */
