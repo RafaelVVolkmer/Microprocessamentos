821ce727278dd34875a96bdf9d2934a225a010c0
RafaelVolkmer <Rafael.V.Volkmer@gmail.com>
2022-10-18 00:38:33 -0300

    Update TecladoMatricial.h

--- I/Trabalho Multiplexação/TecladoMatricial.h
+++ I/Trabalho Multiplexação/TecladoMatricial.h
@@ -1,17 +1,14 @@
-
 #ifndef TECLADOMATRICIAL_H_
 #define TECLADOMATRICIAL_H_
 
-
 #include "stm32f4xx.h"
 
-#define Pinos_Colunas 0b11110000
+#define Pinos_Colunas 0b1110000
 #define Pinos_Linhas 0b1111
 
 #define Primeira_Coluna 0b11100000
 #define Segunda_Coluna 0b11010000
 #define Terceira_Coluna 0b10110000
-#define Quarta_Coluna 0b01110000
 
 #define Primeira_Linha 0b1110
 #define Segunda_Linha 0b1101
@@ -25,7 +22,7 @@ char VarreduraTeclado ()
 
 	char tecla = Nenhuma_Tecla;
 
-	uint16_t i = 0;
+	static uint16_t i = 0;
 
 	GPIOB->ODR |= Pinos_Colunas;
 
@@ -35,24 +32,36 @@ char VarreduraTeclado ()
 
 	if(i != Pinos_Linhas)
 	{
+
 		switch(i)
 		{
+
 		case Primeira_Linha:
+
 			tecla = '1';
-			break;
+
+				break;
 
 		case Segunda_Linha:
+
 			tecla = '4';
-			break;
+
+				break;
 
 		case Terceira_Linha:
+
 			tecla = '7';
-			break;
+
+				break;
 
 		case Quarta_Linha:
-			tecla='*';
-			break;
+
+			tecla = 'P';
+
+				break;
+
 		}
+
 	}
 
 	GPIOB->ODR |= Pinos_Colunas;
@@ -63,25 +72,36 @@ char VarreduraTeclado ()
 
 	if(i != Pinos_Linhas)
 	{
+
 		switch(i)
 		{
 
 		case Primeira_Linha:
+
 			tecla = '2';
-			break;
+
+				break;
 
 		case Segunda_Linha:
+
 			tecla = '5';
-			break;
+
+				break;
 
 		case Terceira_Linha:
+
 			tecla = '8';
-			break;
+
+				break;
 
 		case Quarta_Linha:
+
 			tecla = '0';
-			break;
+
+				break;
+
 		}
+
 	}
 
 	GPIOB->ODR |= Pinos_Colunas;
@@ -92,9 +112,12 @@ char VarreduraTeclado ()
 
 	if(i != Pinos_Linhas)
 	{
+
 		switch(i)
 		{
+
 			case Primeira_Linha:
+
 				tecla = '3';
 
 					break;
@@ -105,41 +128,17 @@ char VarreduraTeclado ()
 					break;
 
 			case Terceira_Linha:
+
 				tecla = '9';
+
 					break;
 
-		case Quarta_Linha:
-			tecla='#';
-			break;
-		}
-	}
+			case Quarta_Linha:
 
-	GPIOB->ODR |= Pinos_Colunas;
+				tecla = 'E';
 
-	GPIOB->ODR &= Quarta_Coluna;
+					break;
 
-	i = GPIOB->IDR & Pinos_Linhas;
-
-	if(i != Pinos_Linhas)
-	{
-		switch(i)
-		{
-		case Primeira_Linha:
-			tecla = 'A';
-
-			break;
-
-		case Segunda_Linha:
-			tecla = 'B';
-			break;
-
-		case Terceira_Linha:
-			tecla = 'C';
-			break;
-
-		case Quarta_Linha:
-			tecla = 'D';
-			break;
 		}
 	}
 
